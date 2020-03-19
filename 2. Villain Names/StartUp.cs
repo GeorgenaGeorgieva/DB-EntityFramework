27768f45c5ec8c247fb3837a174edf5395d9a83b
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-19 19:58:17 +0200

    DB

---
+++ 2. Villain Names/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _2._Villain_Names
+{
+    class StartUp
+    {
+        private static string connectionString =
+            "Server=GEORGENA-PC\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            using (connection)
+            {
+                string queryText = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                     FROM Villains AS v 
+                                     JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                     GROUP BY v.Id, v.Name 
+                                     HAVING COUNT(mv.VillainId) > 3 
+                                     ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand command = new SqlCommand(queryText, connection);
+
+                SqlDataReader readResult = command.ExecuteReader();
+
+                using (readResult)
+                {
+                    while (readResult.Read())
+                    {
+                        Console.WriteLine($"{readResult["Name"]} - {readResult["MinionsCount"]}");
+                    }
+                }
+            }
+        }
+    }
+}
