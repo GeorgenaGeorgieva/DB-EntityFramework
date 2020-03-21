5698c39aea1aa8931eb473583f30276e7f286627
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-21 23:27:50 +0200

    DB

---
+++ 9. Increase Age Stored Procedure/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace _9._Increase_Age_Stored_Procedure
+{
+    class StartUp
+    {
+        private static string connectionString = "" +
+            "Server=GEORGENA-PC\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            int[] inputIds = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var id in inputIds)
+            {
+                UpdateMinionAge(id);
+            }
+
+            foreach (var id in inputIds)
+            {
+                PrintingCurrentMinion(id);
+            }
+        }
+
+        private static void PrintingCurrentMinion(int id)
+        {
+            string queryFindMinion = "SELECT Name, Age FROM Minions WHERE Id = @Id";
+
+            SqlCommand commandFindMinion = new SqlCommand(queryFindMinion, connection);
+
+            using (commandFindMinion)
+            {
+                commandFindMinion.Parameters.AddWithValue("Id", id);
+                SqlDataReader reader = commandFindMinion.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} – {reader["Age"]} years old");
+                    }
+                }
+            }
+        }
+
+        private static void UpdateMinionAge(int id)
+        {
+            string queryExecuteProcedure = @"EXEC usp_GetOlder @id";
+
+            SqlCommand commandExecuteProcedure = new SqlCommand(queryExecuteProcedure, connection);
+
+            using (commandExecuteProcedure)
+            {
+                commandExecuteProcedure.Parameters.AddWithValue("id", id);
+                int affectedRows = commandExecuteProcedure.ExecuteNonQuery();
+
+                Console.WriteLine($"({affectedRows} rows affected)");
+            }
+        }
+    }
+}
