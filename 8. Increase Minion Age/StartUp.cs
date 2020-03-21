8eb7b54ea6bf9ad2824ac8e8e8249c5d9f5bc23c
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-21 23:23:19 +0200

    DB

---
+++ 8. Increase Minion Age/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace _8._Increase_Minion_Age
+{
+    class StartUp
+    {
+        private static string connectionString = "" +
+            "Server=GEORGENA-PC\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            int[] inputMinionIds = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            foreach (var id in inputMinionIds)
+            {
+                UpdateMinionsTableFromDB(id);
+            }
+
+            PrintingAllMinionsFromDatabase();
+        }
+
+        private static void UpdateMinionsTableFromDB(int minionId)
+        {
+            string queryUpdateNameAndAge = @"UPDATE Minions
+                                             SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                             WHERE Id = @Id";
+
+            SqlCommand commandUpdateNameAndAge = new SqlCommand(queryUpdateNameAndAge, connection);
+
+            using (commandUpdateNameAndAge)
+            {
+                commandUpdateNameAndAge.Parameters.AddWithValue("@Id", minionId);
+                int affectedRows = commandUpdateNameAndAge.ExecuteNonQuery();
+
+                Console.WriteLine($"({affectedRows} rows affected)");
+            }
+        }
+
+        private static void PrintingAllMinionsFromDatabase()
+        {
+            string queryFindAllMinions = "SELECT Name, Age FROM Minions";
+
+            SqlCommand commandFindAllMinions = new SqlCommand(queryFindAllMinions, connection);
+
+            using (commandFindAllMinions)
+            {
+                SqlDataReader reader = commandFindAllMinions.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} {reader["Age"]}");
+                    }
+                }
+            }
+        }
+    }
+}
