049c5bb3e468e3a4fa09df2c6b6d1cf68564384a
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-21 23:22:43 +0200

    DB

---
+++ 7. Print All Minion Names/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace _7._Print_All_Minion_Names
+{
+    class StartUp
+    {
+        private static string connectionString = "" +
+            "Server=GEORGENA-PC\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            List<string> minionNames = new List<string>();
+
+            ЕxtractionMinionNamesFromDB(minionNames);
+
+            PrintAllMinionNamesInSpecificOrder(minionNames);
+        }
+
+        private static void ЕxtractionMinionNamesFromDB(List<string> minionNames)
+        {
+            connection.Open();
+
+            string queryFindMinionNames = "SELECT Name FROM Minions";
+
+            SqlCommand commandFindMinionNames = new SqlCommand(queryFindMinionNames, connection);
+
+            using (commandFindMinionNames)
+            {
+                SqlDataReader reader = commandFindMinionNames.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        minionNames.Add((string)reader["Name"]);
+                    }
+                }
+            }
+        }
+
+        private static void PrintAllMinionNamesInSpecificOrder(List<string> minionNames)
+        {
+            for (int i = 0; i < minionNames.Count / 2; i++)
+            {
+                Console.WriteLine(minionNames[i]);
+                Console.WriteLine(minionNames[minionNames.Count - i - 1]);
+            }
+
+            if (minionNames.Count % 2 != 0)
+            {
+                Console.WriteLine(minionNames[minionNames.Count / 2]);
+            }
+        }
+    }
+}
