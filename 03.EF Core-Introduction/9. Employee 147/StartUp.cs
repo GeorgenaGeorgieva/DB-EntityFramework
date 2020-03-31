e2fd6ae6fcf6590f8dfa776bdf1a31a453e68a67
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-31 19:21:44 +0300

    EF

---
+++ 03.EF Core-Introduction/9. Employee 147/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = GetEmployee147(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    EmployeeFullName = e.FirstName + " " + e.LastName,
+                    e.JobTitle,
+                    Projects = e.EmployeesProjects.Select(p => new
+                                            {
+                                                ProjectName = p.Project.Name
+                                            })
+                                            .OrderBy(p => p.ProjectName)
+                })
+                .ToList();
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var emp in employees)
+            {
+                stringBuilder.AppendLine($"{emp.EmployeeFullName} - {emp.JobTitle}");
+
+                foreach (var project in emp.Projects)
+                {
+                    stringBuilder.AppendLine(project.ProjectName);
+                }
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
