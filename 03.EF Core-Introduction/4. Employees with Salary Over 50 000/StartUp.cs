d5113fb86330992f71a0f1828b2255014ad1852e
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-31 19:10:36 +0300

    EF

---
+++ 03.EF Core-Introduction/4. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                stringBuilder.AppendLine($"{e.FirstName} - {e.Salary:F2}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
