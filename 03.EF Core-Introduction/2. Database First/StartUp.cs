886de8aad73c9e313a31a26c98f68a7aebdade70
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-31 19:08:38 +0300

    EF

---
+++ 03.EF Core-Introduction/2. Database First/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+        }
+    }
+}
