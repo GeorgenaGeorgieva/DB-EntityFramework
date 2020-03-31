5f45d663ab745a3ad2bb611b0c8b1027c45d2a5f
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-31 19:09:56 +0300

    EF

---
+++ 03.EF Core-Introduction/3. Employees Full Information/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{ 
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context)); 
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary,
+                    e.EmployeeId
+                })
+                .OrderBy(e => e.EmployeeId)
+                .ToList();
+
+            foreach (var empl in employees)
+            {
+                stringBuilder.AppendLine($"{empl.FirstName} {empl.LastName} " +
+                    $"{empl.MiddleName} {empl.JobTitle} {empl.Salary:F2}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
