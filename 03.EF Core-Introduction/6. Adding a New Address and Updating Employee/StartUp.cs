26f133128be63c674c2db06090786ef3b125e131
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-31 19:17:04 +0300

    EF

---
+++ 03.EF Core-Introduction/6. Adding a New Address and Updating Employee/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = AddNewAddressToEmployee(context);
+            Console.WriteLine(result);
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var address = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            var employeeNakov = context.Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+
+            employeeNakov.Address = address;
+
+            context.SaveChanges();
+
+            var employees = context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => new
+                {
+                    AddressText = e.Address.AddressText
+                })
+                .ToList();
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                stringBuilder.AppendLine(e.AddressText);
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
