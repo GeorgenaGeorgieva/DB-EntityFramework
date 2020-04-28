83cf9ebf2cfc07e7177cbef97c4dcff3be262bc2
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-04-28 23:50:39 +0300

    EF Core

---
+++ 03.EF Core-Introduction/12. Increase Salaries/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = IncreaseSalaries(context);
+            Console.WriteLine(result);
+        }
+
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Engineering"
+                         || e.Department.Name == "Tool Design"
+                         || e.Department.Name == "Marketing "
+                         || e.Department.Name == "Information Services")
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                employee.Salary *= 1.12m;
+            }
+
+            var selectedEmployeesInformation = employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var employee in selectedEmployeesInformation)
+            {
+                stringBuilder.AppendLine($"{employee.FirstName} {employee.LastName} (${employee.Salary:F2})");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
