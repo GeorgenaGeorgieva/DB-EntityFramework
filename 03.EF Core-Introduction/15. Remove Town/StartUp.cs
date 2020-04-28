70316584e3d2ec649a55359f66298d4d72f132f1
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-04-28 23:56:42 +0300

    EF Core

---
+++ 03.EF Core-Introduction/15. Remove Town/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = RemoveTown(context);
+            Console.WriteLine(result);
+        }
+
+        public static string RemoveTown(SoftUniContext context)
+        {
+            var townForDelete = context.Towns
+                .FirstOrDefault(t => t.Name == "Seattle");
+
+            var addressForDelete = context.Addresses
+                .Where(a => a.Town.Name == "Seattle");
+
+            var countOfDeletedAddresses = addressForDelete.Count();
+
+            context.Employees
+                .Where(e => e.Address.Town.Name == "Seattle")
+                .ToList()
+                .ForEach(a => a.AddressId = null);
+
+            context.Addresses.RemoveRange(addressForDelete);
+            context.Towns.Remove(townForDelete);
+
+            context.SaveChanges();
+
+            return $"{countOfDeletedAddresses} addresses in Seattle were deleted";
+        }
+    }
+}
---
+++ 03.EF Core-Introduction/Introduction to EF Database First/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
