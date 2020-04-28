5c947c90805f275a20100ab7948c846bd31e56fc
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-04-28 23:52:49 +0300

    EF Core

---
+++ 03.EF Core-Introduction/14. Delete Project by Id/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = DeleteProjectById(context);
+            Console.WriteLine(result);
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            var projectForDelete = context.Projects
+                .FirstOrDefault(p => p.ProjectId == 2);
+
+            var employeeProjectForDelete = context.EmployeesProjects
+                .Where(p => p.ProjectId == 2);
+
+            context.EmployeesProjects.RemoveRange(employeeProjectForDelete);
+            context.Projects.Remove(projectForDelete);
+
+            context.SaveChanges();
+
+            var projects = context.Projects
+                .Select(p => new
+                {
+                    p.Name
+                })
+                .ToList();
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var project in projects)
+            {
+                stringBuilder.AppendLine(project.Name);
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
