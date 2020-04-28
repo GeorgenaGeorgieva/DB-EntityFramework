e4308f399b4dbaca59af8565177fabd2ce30acbc
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-04-28 23:47:23 +0300

    EF Core

---
+++ 03.EF Core-Introduction/10. Departments with More Than 5 Employees/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = GetDepartmentsWithMoreThan5Employees(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var departments = context.Departments
+                .Select(d => new
+                {
+                    d.Name,
+                    ManagerFirstName = d.Manager.FirstName, 
+                    ManagerLastName = d.Manager.LastName,
+                    Employees = d.Employees.Select(e => new
+                    {
+                        e.FirstName,
+                        e.LastName,   
+                        e.JobTitle
+                    })
+                    .OrderBy(e => e.FirstName)
+                    .ThenBy(e => e.LastName)
+                    .ToList()
+                })
+                .Where(d => d.Employees.Count > 5)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .ToList();
+
+            foreach (var depart in departments)
+            {
+                stringBuilder.AppendLine($"{depart.Name} - {depart.ManagerFirstName} {depart.ManagerLastName}");
+
+                foreach (var emp in depart.Employees)
+                {
+                    stringBuilder.AppendLine($"{emp.FirstName} {emp.LastName} - {emp.JobTitle}");
+                }
+            }
+            
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
